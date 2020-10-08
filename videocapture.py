c12bb030c2dba5b80cbc71ef48216b858f26bcb7
kartikeya Sharma <56194329+savagecarol@users.noreply.github.com>
2020-10-08 18:45:36 +0530

    new code

--- videocapture.py
+++ videocapture.py
@@ -1,10 +1,19 @@
-import cv2
 
-vc = cv2.VideoCapture(0)
-while(True):
-    ret, frame = vc.read()
-    cv2.imshow("You're LIVE", frame)
-    if cv2.waitKey(1) == 13:
+import cv2
+cap=cv2.VideoCapture(0)
+count=0
+while True:
+    ret,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    crop=gray[:300,:300]
+    if ret:
+        cv2.imshow("window",gray)
+        cv2.imshow("crop",crop)
+    key=cv2.waitKey(1)
+    if ord('q')==0xff & key:
         break
-vc.release()
+    if ord('c') == 0xff & key:
+        cv2.imwrite("{}.png".format(count),frame)
+        count+=1
+cap=release()
 cv2.destroyAllWindows()

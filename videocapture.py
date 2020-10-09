5f03dcaa357068fc6b30d7bbe7f4b4404b4827fb
kartikeya Sharma <56194329+savagecarol@users.noreply.github.com>
2020-10-09 21:55:49 +0530

    it is right

--- videocapture.py
+++ videocapture.py
@@ -1,19 +1,16 @@
-
 import cv2
 cap=cv2.VideoCapture(0)
 count=0
 while True:
     ret,frame=cap.read()
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    crop=gray[:300,:300]
     if ret:
-        cv2.imshow("window",gray)
-        cv2.imshow("crop",crop)
+        cv2.imshow("window",frame)
+
     key=cv2.waitKey(1)
     if ord('q')==0xff & key:
         break
     if ord('c') == 0xff & key:
         cv2.imwrite("{}.png".format(count),frame)
         count+=1
-cap=release()
+cap=cv2.release()
 cv2.destroyAllWindows()
